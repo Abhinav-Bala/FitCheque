ca8c23af610bee3820d7a179cfdd4b0bf102e8d0
abhinav-bala <abhinav.bala24@gmail.com>
2022-12-19 11:40:52 -0500

    created frontend boilerplate

---
+++ frontend/fit-cheque/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Home from './components/homepage/Home'
+function App() {
+  return (
+    <div className="min-h-screen min-w-screen">
+      <Home/>
+    </div>
+  )
+}
+
+export default App
