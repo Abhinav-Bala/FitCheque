86656180189a2738e8ddb315065ed91a34f8e1dd
abhinav-bala <abhinav.bala24@gmail.com>
2022-12-19 11:27:07 -0500

    homepage changes

--- client/fit-cheque/src/components/HomePage.js
+++ client/fit-cheque/src/components/HomePage.js
@@ -7,12 +7,14 @@ import {Link} from 'react-router-dom';
 
 const Homepage = () => {
   const [roomCode, setRoomCode] = useState('')
+  const [error, setError] = useState(false)
 
   const validateRoomCode = (roomCode) => {
-    if(roomCode === ''){
-      console.log('wrong')
-    } else {
-      return roomCode
+    //replace with try catch later
+
+    if(roomCode !== '000' || roomCode !== ''){
+      setError(true)
+      setRoomCode('')
     }
   }
 
@@ -25,10 +27,14 @@ const Homepage = () => {
           <div className='p-24 bg-green-300 m-auto grid-cols-1 flex flex-col space-y-4'>
             <h1 className='text-7xl pb-6'>Fit Cheque</h1>
             <div className='flex flex-col space-y-2'>
-              <input className='text-center homepage-btn' placeholder='enter game code' onChange={event => setRoomCode(event.target.value)}></input>
-              <Link to={`/play?room=${validateRoomCode(roomCode)}`} className='bg-yellow-200 homepage-btn text-center'>
-              <button >join</button>
-              </Link>   
+              <input className='text-center homepage-btn' placeholder='enter game code' onChange={event => setRoomCode(event.target.value)} value={roomCode}></input>
+              {error ? (
+                <p claseName='text-sm text-center' role="alert" style={{ color: "rgb(255, 0, 0)" }}>
+                  Please make sure you've entered a valid room code
+                </p>
+              ) : <div className='h-8'/>}
+              <button className='bg-yellow-200 homepage-btn text-center'
+              onClick={validateRoomCode}>join</button>
             </div>
             <h3 className='text-md font-semibold text-center'>OR</h3>
             <Link className='bg-purple-200 homepage-btn text-center' to={`/play?room=${createId(6)}`}>
